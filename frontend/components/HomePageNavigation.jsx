01fd39a347689b235f74cdf442f5850a3d4e8e6e
Collins Matela <matela2nd@gmail.com>
2026-03-27 22:53:36 +0800

    Home Page UI

---
+++ frontend/components/HomePageNavigation.jsx
@@ -0,0 +1,10 @@
+
+const HomePageNavigation = () => {
+    return(
+        <nav className="bg-white h-20 w-full fixed top-0 z-[9999] justify-between items-center flex px-20">
+             <h1>Online Library System</h1>
+             <div className="bg-black p-2 rounded-full w-20 text-white justify-center items-center flex cursor-pointer">Login</div>
+        </nav>
+    )
+}
+export default HomePageNavigation;
--- frontend/pages/Home_Page.jsx
+++ frontend/pages/Home_Page.jsx
@@ -1,9 +1,17 @@
-
+import Navigation from "../components/HomePageNavigation"
+import Footer from "../components/Footer"
 const Home_Page = () => {
     return(
-        <section className="h-screen w-full bg-black">
-            <h1 className="text-white">Fuck all</h1>
+        <>       
+        <section className="min-h-screen w-full bg-white justify-center items-center flex flex-col px-20 gap-4">
+            <Navigation/>
+            <div className="border-2  rounded-xl px-2">✨ Online Library System</div>
+            <h1 className="text-black text-8xl">Read, Play, and Learn Every Day!</h1>
+            <h1 className="text-black text-lg">A fun and colorful place where little learners can explore stories, learn new things, and grow their imagination.</h1>
+            <div className="bg-black justify-center items-center flex py-2 px-4 text-white rounded-full gap-2 cursor-pointer"> Get Started ➤ </div>
         </section>
+        <Footer/>
+        </>
     )    
 }
 export default Home_Page;

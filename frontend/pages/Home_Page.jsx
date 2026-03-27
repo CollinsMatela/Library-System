dffa284cdc3a0fc2a0ca8c2a7cc9a8092a1e84f0
Collins Matela <matela2nd@gmail.com>
2026-03-27 11:53:04 +0800

    home page

---
+++ frontend/pages/Home_Page.jsx
@@ -0,0 +1,9 @@
+
+const Home_Page = () => {
+    return(
+        <section className="h-screen w-full bg-black">
+            <h1 className="text-white">Fuck all</h1>
+        </section>
+    )    
+}
+export default Home_Page;

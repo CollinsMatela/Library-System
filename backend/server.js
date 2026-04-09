572ef43d38387bb893d8965625a4a276aa99dfba
Collins Matela <matela2nd@gmail.com>
2026-04-09 11:39:35 +0800

    working on RegisterStudentModal and setup backend folder

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- frontend/modals/RegisterStudentModal.jsx
+++ frontend/modals/RegisterStudentModal.jsx
@@ -1,7 +1,49 @@
+import { use, useState } from "react";
 
 const RegisterStudentModal = ({ onClose }) => {
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth();
+
+    const [lastname, setLastname] = useState("")
+    const [firstname, setFirstname] = useState("")
+    const [middlename, setMiddlename] = useState("")
+    const [year, setYear] = useState("")
+    const [month, setMonth] = useState("")
+    const [day, setDay] = useState("")
+    const [age, setAge] = useState("")
+    const [gender, setGender] = useState("")
+    const [parentLastname, setParentLastname] = useState("")
+    const [parentFirstname, setParentFirstname] = useState("")
+    const [parentMiddlename, setParentMiddlename] = useState("")
+    const [parentEmail, setParentEmail] = useState("")
+    const [parentContact, setParentContact] = useState("")
+    const [parentRelationship, setParentRelationship] = useState("")
+    const [gradeLevel, setGradeLevel] = useState("")
+
+    const handleStudentRegistration = () => {
+        const studentInformation = {
+             lasname: lastname,
+             firstname: firstname,
+             middlename: middlename,
+             year: year,
+             month: month,
+             day: day,
+             age: age,
+             gender: gender,
+             parentLastname: parentLastname,
+             parentFirstname: parentFirstname,
+             parentMiddlename: parentMiddlename,
+             parentEmail: parentEmail,
+             parentContact: parentContact,
+             parentRelationship: parentRelationship,
+             gradeLevel: gradeLevel
+        }
+        try {
+            console.log("Student Information:", studentInformation);
+        } catch (error) {
+            console.log("Error registering student account:", error);
+        }
+    }
 
     return(
         <section className="fixed inset-0 justify-center items-center flex">
@@ -16,15 +58,15 @@ const RegisterStudentModal = ({ onClose }) => {
                         <h1 className="text-sm font-bold text-gray-500">Student Information</h1>
                         <div className="w-full">
                             <h1 className="text-xs text-gray-500">Last Name <span className="text-red-500">*</span></h1>
-                            <input type="text" placeholder="Last Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4"/>
+                            <input type="text" placeholder="Last Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4" value={lastname} onChange={(e) => setLastname(e.target.value)} />
                         </div>
                         <div className="w-full">
                             <h1 className="text-xs text-gray-500">First Name <span className="text-red-500">*</span></h1>
-                            <input type="text" placeholder="First Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4"/>
+                            <input type="text" placeholder="First Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
                         </div>
                         <div className="w-full">
                             <h1 className="text-xs text-gray-500">Middle Name <span className="text-red-500">*</span></h1>
-                            <input type="text" placeholder="Middle Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4"/>
+                            <input type="text" placeholder="Middle Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4" value={middlename} onChange={(e) => setMiddlename(e.target.value)} />
                         </div>
                         
                         <div className="w-full justify-center items-center flex gap-2">
@@ -32,14 +74,14 @@ const RegisterStudentModal = ({ onClose }) => {
                             <div className="w-full justify-center items-start flex flex-col">
                                 <h1 className="text-xs text-gray-500">Date of birth <span className="text-red-500">*</span></h1>
                                 <div className="justify-center items-center flex gap-2">
-                                    <select name="Year" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-2 text-gray-500">
+                                    <select name="Year" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-2 text-gray-500" value={year} onChange={(e) => setYear(e.target.value)}>
                                         <option value="">Year</option>
                                         {Array.from({ length: currentYear - 1999 }, (currentValue, index) => {
                                             const year = currentYear - index;
                                             return <option key={year} value={year}>{year}</option>;
                                         })}
                                     </select>
-                                    <select name="Year" className="border-1 border-gray-300 h-12 w-35 outline-none rounded-xl px-2 text-gray-500">
+                                    <select name="Month" className="border-1 border-gray-300 h-12 w-35 outline-none rounded-xl px-2 text-gray-500" value={month} onChange={(e) => setMonth(e.target.value)}>
                                         <option value="">Month</option>
                                         <option value="1">January</option>
                                         <option value="2">February</option>
@@ -54,7 +96,7 @@ const RegisterStudentModal = ({ onClose }) => {
                                         <option value="11">November</option>
                                         <option value="12">December</option>
                                     </select>
-                                    <select name="Year" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-2 text-gray-500">
+                                    <select name="Day" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-2 text-gray-500" value={day} onChange={(e) => setDay(e.target.value)}>
                                         {Array.from({ length: 31 }, (currentValue, index) => {
                                             const day = index + 1;
                                             return <option key={day} value={day}>{day}</option>;
@@ -65,7 +107,7 @@ const RegisterStudentModal = ({ onClose }) => {
                             
                             <div>
                                 <h1 className="text-xs text-gray-500">Age<span className="text-red-500">*</span></h1>
-                                <input type="text" placeholder="Age" className="bg-gray-100 h-12 w-20 outline-none rounded-xl px-4 text-gray-500 cursor-not-allowed" disabled/>
+                                <input type="text" placeholder="Age" className="bg-gray-100 h-12 w-20 outline-none rounded-xl px-4 text-gray-500 cursor-not-allowed" disabled value={age} onChange={(e) => setAge(e.target.value)}/>
                             </div>
                             
                         </div>
@@ -73,7 +115,7 @@ const RegisterStudentModal = ({ onClose }) => {
                         
                         <div className="w-full">
                             <h1 className="text-xs text-gray-500">Gender <span className="text-red-500">*</span></h1>
-                            <select className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4 text-gray-500">
+                            <select className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4 text-gray-500" value={gender} onChange={(e) => setGender(e.target.value)}>
                             <option value="Male">Male</option>
                             <option value="Female">Female</option>
                             </select>
@@ -86,29 +128,29 @@ const RegisterStudentModal = ({ onClose }) => {
                         <h1 className="text-sm font-bold text-gray-500">Parent/Guardian Information</h1>
                         <div className="w-full">
                             <h1 className="text-xs text-gray-500">Last Name <span className="text-red-500">*</span></h1>
-                            <input type="text" placeholder="Last Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4"/>
+                            <input type="text" placeholder="Last Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4" value={parentLastname} onChange={(e) => setParentLastname(e.target.value)}/>
                         </div>
                         <div className="w-full">
                             <h1 className="text-xs text-gray-500">First Name <span className="text-red-500">*</span></h1>
-                            <input type="text" placeholder="First Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4"/>
+                            <input type="text" placeholder="First Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4" value={parentFirstname} onChange={(e) => setParentFirstname(e.target.value)}/>
                         </div>
                         <div className="w-full">
                             <h1 className="text-xs text-gray-500">Middle Name <span className="text-red-500">*</span></h1>
-                            <input type="text" placeholder="Middle Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4"/>
+                            <input type="text" placeholder="Middle Name" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4" value={parentMiddlename} onChange={(e) => setParentMiddlename(e.target.value)}/>
                         </div>
                         <div className="w-full flex gap-2">
                             <div className="flex-1">
                                 <h1 className="text-xs text-gray-500">Email Address <span className="text-red-500">*</span></h1>
-                                <input type="text" placeholder="Email Address" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4"/>
+                                <input type="text" placeholder="Email Address" className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4" value={parentEmail} onChange={(e) => setParentEmail(e.target.value)}/>
                             </div>
                             <div className="flex-1">
                                 <h1 className="text-xs text-gray-500">Contact No. <span className="text-red-500">*</span></h1>
-                                <input type="text" placeholder="Contact No." className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4"/>
+                                <input type="text" placeholder="Contact No." className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4" value={parentContact} onChange={(e) => setParentContact(e.target.value)}/>
                             </div>
                         </div>
                         <div className="w-full">
                             <h1 className="text-xs text-gray-500">Relationship <span className="text-red-500">*</span></h1>
-                            <select className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4 text-gray-500">
+                            <select className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4 text-gray-500" value={parentRelationship} onChange={(e) => setParentRelationship(e.target.value)}>
                                 <option value="">Select Relationship</option>
                                 <option value="Father">Father</option>
                                 <option value="Mother">Mother</option>
@@ -121,8 +163,23 @@ const RegisterStudentModal = ({ onClose }) => {
                     </div>
                     </div>
                     
+                    <div className="w-full justify-start items-start flex flex-col gap-2">
+                        <h1 className="text-sm font-bold text-gray-500">School Information</h1>
+                        <div className="w-full">
+                            <h1 className="text-xs text-gray-500">Grade Level <span className="text-red-500">*</span></h1>
+                             <select className="border-1 border-gray-300 h-12 w-full outline-none rounded-xl px-4 text-gray-500" value={gradeLevel} onChange={(e) => setGradeLevel(e.target.value)}>
+                                <option value="">Select Grade Level</option>
+                                <option value="Kindergarten">Kindergarten</option>
+                                <option value="Grade 1">Grade 1</option>
+                                <option value="Grade 2">Grade 2</option>
+                                <option value="Grade 3">Grade 3</option>
+                                <option value="Grade 4">Grade 4</option>
+                        </select>
+                        </div>
+                    </div>
+
                     <div className="w-full justify-end items-center flex">
-                        <button className="bg-blue-600 text-white py-2 px-4 rounded-full hover:bg-blue-700 cursor-pointer font-bold text-sm">Register</button>
+                        <button className="bg-blue-600 text-white py-2 px-4 rounded-full hover:bg-blue-700 cursor-pointer font-bold text-sm" onClick={handleStudentRegistration}>Register</button>
                     </div>
                </div>
         </section>

683bd87acd89f4745c3c2360e47007b0f09f9ef5
maku <markicban72@gmail.com>
2026-04-26 19:17:13 +0800

    homepage design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,16 @@ import Library_Page from '../pages/Library_Page'
 import Admin_Upload_Page from "../pages/Admin_Upload_Page";
 
 function App() {
-
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home_Page/>}/>
         <Route path="/library" element={<Library_Page/>}/>
         <Route path="/admin-page" element={<Admin_Page/>}/>
         <Route path="/admin-page/upload" element={<Admin_Upload_Page/>}/>
       </Routes>
-      </BrowserRouter>
-
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;

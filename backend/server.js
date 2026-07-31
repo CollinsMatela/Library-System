dbf6c869a90971d983dfad655f35f4afee56b4b1
unknown <matela2nd@gmail.com>
2026-07-31 15:23:59 +0800

    configure for vercel

--- backend/server.js
+++ backend/server.js
@@ -122,6 +122,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
   
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });

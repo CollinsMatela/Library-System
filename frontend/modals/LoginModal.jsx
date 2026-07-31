4833a974ee83ff025a1106550a580544efc2403a
unknown <matela2nd@gmail.com>
2026-07-31 21:43:19 +0800

    added loading in loggin in

--- frontend/modals/LoginModal.jsx
+++ frontend/modals/LoginModal.jsx
@@ -24,7 +24,7 @@ const LoginModal = ({ onClose }) => {
   const [isErrorContainer, setIsErrorContainer] = useState(false);
   const [Message, setIsMessage ] = useState("");
 
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const confirmation = () => {
        if(username === "") {
@@ -42,6 +42,8 @@ const LoginModal = ({ onClose }) => {
   }
 
   const loginAccount = async () => {
+        setIsLoading(true);
+
         const account = {
           username: username,
           password: password
@@ -74,16 +76,19 @@ const LoginModal = ({ onClose }) => {
 
           toast.success(res.data.message);
           }
+          
         } catch (error) {
           setIsMessage(error.response?.data?.message || "Login failed. Please try again.");
           toast.warning(error?.response?.data?.message);
           setIsErrorContainer(true);
+        } finally {
+          setIsLoading(false);
         }
   }
 
   return (
     <section className="fixed inset-0 flex justify-center items-center z-100">
-      {loading && (<LoadingScreen/>)}
+
       <div
         className="absolute inset-0 bg-black/50 z-0"
       ></div>
@@ -105,7 +110,7 @@ const LoginModal = ({ onClose }) => {
 
         <div className={`w-full justify-center items-start flex flex-col mb-2`}>
           <h1 className="text-xs text-gray-500">Email</h1>
-          <input type="text" placeholder="Your Email" className={`${isUsername ? "border-red-500" : "border-gray-300"} border p-2 text-xs w-full rounded-lg p-2 outline-none`}
+          <input type="text" placeholder="Your Email" className={`${isUsername ? "border-red-500" : "border-gray-300"} border p-2 text-xs w-full rounded-lg outline-none`}
           value={username} onChange={(e) => {setUsername(e.target.value)
                                              if(username){setIsUsername(false)}
           }}/>
@@ -113,13 +118,17 @@ const LoginModal = ({ onClose }) => {
 
         <div className="w-full justify-center items-start flex flex-col">
           <h1 className="text-xs text-gray-500">Password</h1>
-          <input type="password" placeholder="Your Password" className={`${isPassword ? "border-red-500" : "border-gray-300"} border p-2 text-xs w-full rounded-lg p-2 outline-none`}
+          <input type="password" placeholder="Your Password" className={`${isPassword ? "border-red-500" : "border-gray-300"} border p-2 text-xs w-full rounded-lg outline-none`}
           value={password} onChange={(e) => {setPassword(e.target.value)
                                              if(password){setIsPassword(false)}
           }}/>
         </div>
 
-        <button className="bg-blue-600 p-2 w-full text-white text-sm rounded-full cursor-pointer outline-none hover:bg-blue-700 mt-6" onClick={() => confirmation()}>Sign Up</button>
+        <button 
+        className={`p-2 w-full text-white text-sm rounded-full cursor-pointer outline-none hover:bg-blue-700 mt-6 ${isLoading ? 'bg-gray-500' : "bg-blue-600"}`} 
+        onClick={() => confirmation()}>
+          {isLoading ? 'Logging in...' : 'Login'}
+        </button>
         <button className="bg-white border border-gray-300 p-2 w-full text-gray-500 text-sm rounded-full cursor-pointer outline-none hover:bg-gray-100 mt-2" onClick={onClose}>Cancel</button>
 
       </div>

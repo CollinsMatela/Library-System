5c207bbcb40959d2bd58c1ba70603330bad9002d
unknown <matela2nd@gmail.com>
2026-08-01 12:12:05 +0800

    deleted port

--- backend/server.js
+++ backend/server.js
@@ -109,10 +109,4 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB connection failed:", err));
   
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 export default app;

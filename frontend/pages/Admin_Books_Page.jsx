5866066dd231d0e1fae7789aca65786f9e51fc2a
unknown <matela2nd@gmail.com>
2026-08-31 16:03:07 +0800

    did admin catalog responsive

--- frontend/pages/Admin_Books_Page.jsx
+++ frontend/pages/Admin_Books_Page.jsx
@@ -50,15 +50,15 @@ const Admin_Books_Page = () => {
       return(
         <>
         <Admin_Sidebar/>
-        <section className="bg-white min-h-screen w-full justify-start items-start flex flex-col pl-70">
+        <section className="bg-white min-h-screen w-full justify-start items-start flex flex-col md:pl-20 lg:pl-60">
               
-              <header className="w-full justify-between items-start flex flex-col mb-10 border-b border-stone-300 p-3 px-10">
+              <header className="w-full justify-between items-start flex flex-col mb-10 border-0 lg:border-b border-stone-300 p-3 px-4 lg:px-10">
                     <h1 className="text-sm font-bold text-stone-800">Books Management</h1>
                     <h1 className="text-stone-400 text-xs">Manage uploaded books from library</h1>                   
               </header>
 
-              <div className="w-full justify-between items-start flex rounded-t-xl px-10">
-                        <div className="flex items-center justify-start gap-2 w-full mb-4">
+              <div className="w-full justify-between items-start flex flex-col md:flex-row rounded-t-xl px-4 gap-2 mb-4 lg:px-10">
+                        <div className="flex items-center justify-start gap-2 w-full">
                             <div className="bg-stone-800 p-2 text-white justify-center items-center flex">
                                 <LibraryBig size={20}/>
                             </div>
@@ -69,10 +69,10 @@ const Admin_Books_Page = () => {
                         </div>
                         
 
-                        <div className="justify-between items-center flex border border-stone-300 rounded-lg px-4">
+                        <div className="justify-between items-center flex border border-stone-300 rounded-lg px-4 w-full md:w-100">
                             
                             <input type="search" 
-                                   placeholder="Search by name" 
+                                   placeholder="Search book title" 
                                    className="bg-white py-2 outline-none text-xs"
                                    value={search}
                                    onChange={(e) => setSearch(e.target.value)}
@@ -90,7 +90,7 @@ const Admin_Books_Page = () => {
               )
               :
               (
-                <div className="w-full px-10">
+                <div className="w-full px-4 lg:px-10">
                   {filteredBooks.length === 0 && (
                     <div className="bg-stone-100 w-full rounded-2xl justify-center items-center flex p-4">
                       <h1 className="text-xs text-stone-500">No books uploaded</h1>
@@ -104,28 +104,37 @@ const Admin_Books_Page = () => {
                       >
                       
                       <div className="h-full flex w-full">
-                          <img src={book?.cover} className="object-cover w-25" />
+                          <img src={book?.cover} className="hidden sm:block object-cover w-25" />
 
                           <div className="h-full flex flex-col w-full p-2">
                             <div className="w-full flex gap-2 justify-center items-center mb-2">
-                              <div className="bg-stone-200 shadow-sm p-2 rounded-full">
+                              <div className="hidden md:block bg-stone-200 shadow-sm p-2 rounded-full">
                                 <Book size={15} className="text-stone-500"/>
                               </div>
                               
 
                               <div className="w-full">
-                                <div className="flex justify-between items-center w-full">
-                                  <h1 className="text-stone-800 font-semibold text-sm">{book?.title} <span className="text-stone-400 text-xs"> • {book?.category}</span></h1>
+                                <div className="flex flex-col md:flex-row justify-between items-start w-full">
+
+                                  <div className="justify-center items-center flex gap-2">
+                                  <h1 className="text-stone-800 font-semibold text-sm">{book?.title}</h1>
+                                  <h1 className="text-stone-400 text-xs">By {book?.author}</h1>
+                                  </div>
+
+                                  <div className="justify-center items-center flex gap-2">
+                                  <h1 className="text-stone-500 text-xs font-normal p-1 bg-stone-200 rounded-full"> {book?.category}</h1>
                                   <h1 className={`${book?.copies> 0 ? "text-green-600 bg-green-100" : "text-red-600 bg-red-100"} text-xs p-1 rounded-full`}>{book?.copies> 0 ? "Available" : "Not Available"}</h1>
+                                  </div>
+                                  
                                 </div>
-                                  <h1 className="text-stone-400 text-xs mb-2">{book?.author}</h1>
+                                  
                               </div>
                               
                             </div>
                               
                               
                               
-                              <h1 className="text-stone-400 text-xs">{book?.description.length > 100 ? `${book?.description.slice(0, 200)}...` : book?.description}</h1>
+                              <h1 className="text-stone-400 text-xs overflow-auto">{book?.description.length > 100 ? `${book?.description.slice(0, 200)}...` : book?.description}</h1>
                               
                           </div>
                       </div>
